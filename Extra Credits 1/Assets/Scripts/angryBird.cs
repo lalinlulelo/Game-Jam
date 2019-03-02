1161d2815947cb1799c9c1ddd3fb9639d9624c33
ziyao <ziyaozhan1998@gmail.com>
2019-03-02 16:14:01 +0100

    flyingBird

---
+++ Extra Credits 1/Assets/Scripts/angryBird.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class angryBird : MonoBehaviour
+{
+    private int min = -2;
+    private int max = 2;
+    public float y;
+    private Rigidbody2D rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D=GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody2D.AddForce(new Vector2(0,RandomNumber(min,max)));
+    }
+
+    // Generate a random number between two numbers
+    public int RandomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
+    }
+}
