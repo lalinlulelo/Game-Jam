4746fb8667c9c79aab2226bc90b93e01a8f96401
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-02 11:38:37 +0100

    mover luna con coordenadas dadas

--- Extra Credits 1/Assets/Scripts/moverLuna.cs
+++ Extra Credits 1/Assets/Scripts/moverLuna.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class moverLuna : MonoBehaviour
 {
-    
+    //no se para que sirven
     public float alpha = 0f;
-
     public float tilt = 0f;
+    /*sirven para poner el punto de inicio, es decir, en que coordenadas empieza, 
+     * solo ajustar en caso de querer que empiece en cierto punto, si no lo entiendes
+     * PREGUNTAME!!!!
+     */
+     //copia del unity en transform las coordenadas
+    public float xInicio = 0f;
+    public float yInicio = 0f;
+
+    /*ajustando los dos primeros puedes prolongar horizontalmente, ve probando,
+     los dos que siguen para prolongar verticalmente*/
+    //los valores que he puesto es para que me sirva a mi
+    public float primero = 5f;
+    public float segundo = 10f;
+    public float tercero = 6f;
+    public float cuarto = 5f;
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(-(0f + (1f * MCos(alpha) * MSin(tilt)) + (5f * MSin(alpha) * MCos(tilt))),
-            -(0f + (1f * MCos(alpha) * MCos(tilt)) - (5f * MSin(alpha) * MSin(tilt))));
-        alpha += 0.5f;
+        //modificar el segundo parametro hace que se prolongue horizontalmente, el primero verticalmente
+        transform.position = new Vector2((xInicio + (primero * MCos(alpha) * MSin(tilt)) + (segundo * MSin(alpha) * MCos(tilt))),
+            (yInicio + (tercero * MCos(alpha) * MCos(tilt)) - (cuarto * MSin(alpha) * MSin(tilt))));
+        //modificar ajusta la velocidad, negativo de derecha a izquierda, positivo de izquierda a derecha
+        alpha += -1f;
     }
     float MCos(float value)
     {
