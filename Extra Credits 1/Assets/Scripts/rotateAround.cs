93510efd9e21ebde50eedc18c8934de5fdb4861f
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-02 12:42:48 +0100

    imagen junto a tierra

---
+++ Extra Credits 1/Assets/Scripts/rotateAround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateAround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed = 10;
+    // Update is called once per frame
+    void Update()
+    {
+        //primer parametro donde empieza, segundo ejes, tercero velocidad
+        transform.RotateAround(Vector3.zero, Vector3.forward, speed * Time.deltaTime);
+    }
+}
