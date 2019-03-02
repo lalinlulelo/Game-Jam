53c981014fe9157eef6f662030e18c143e7f8b99
vlt23 <32304181+vlt23@users.noreply.github.com>
2019-03-02 20:15:44 +0100

    vlt23
    [ADD][WIP] Game menu

---
+++ Extra Credits 1/Assets/MainMenu/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : UnityEngine.MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
