caa9df74a48effee062546cddf7e56aa698331da
vlt23 <32304181+vlt23@users.noreply.github.com>
2019-03-02 20:32:45 +0100

    Fix Build Settings? && Maybe fix exit buttom

---
+++ Extra Credits 1/Assets/MainMenu/Scripts/ExitController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
