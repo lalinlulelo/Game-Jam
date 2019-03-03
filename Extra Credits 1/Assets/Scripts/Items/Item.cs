726d40112b48950d0083fdae5219270f7df3f46f
daolao <dalao1002@gmail.com>
2019-03-03 23:03:10 +0100

    orange add health

---
+++ Extra Credits 1/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Item : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        float fixedSpeed = speed * Time.deltaTime;
+        transform.Translate(Vector2.left * fixedSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.SendMessage("ApplyEfect", this.GetType());
+        Destroy(gameObject);
+    }
+}
---
+++ Extra Credits 1/Assets/Scripts/Obstacles/ItemSpanwer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpanwer : MonoBehaviour
+{
+    public Item[] items;
+    public Vector3 direction;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randEnemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+        if (stop == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+            randEnemy = Random.Range(0, this.items.Length);
+
+            Vector3 SpawnPosition = new Vector3(Random.Range(-direction.x, direction.x), Random.Range(-direction.y, direction.y), 1);
+
+            Instantiate(items[randEnemy], SpawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
