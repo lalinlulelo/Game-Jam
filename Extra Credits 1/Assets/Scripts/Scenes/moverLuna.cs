6dd4aead206e51078ac008da1111d987f8ef349e
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-03 12:03:02 +0100

    cambio nombre light

--- Extra Credits 1/Assets/Scripts/Scenes/moverLuna.cs
+++ Extra Credits 1/Assets/Scripts/Scenes/moverLuna.cs
@@ -14,6 +14,7 @@ public class moverLuna : MonoBehaviour
      //copia del unity en transform las coordenadas
     public float xInicio = 0f;
     public float yInicio = 0f;
+    public float zInicio = 0f;
 
     /*ajustando los dos primeros puedes prolongar horizontalmente, ve probando,
      los dos que siguen para prolongar verticalmente*/
@@ -25,9 +26,9 @@ public class moverLuna : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //modificar el segundo parametro hace que se prolongue horizontalmente, el primero verticalmente
-        transform.position = new Vector2((xInicio + (primero * MCos(alpha) * MSin(tilt)) + (segundo * MSin(alpha) * MCos(tilt))),
-            (yInicio + (tercero * MCos(alpha) * MCos(tilt)) - (cuarto * MSin(alpha) * MSin(tilt))));
+        //modificar el segundo parametro hace que se prolongue verticalmente, el primero horizontalmente
+        transform.position = new Vector3((xInicio + (primero * MCos(alpha) * MSin(tilt)) + (segundo * MSin(alpha) * MCos(tilt))),
+            (yInicio + (tercero * MCos(alpha) * MCos(tilt)) - (cuarto * MSin(alpha) * MSin(tilt))),zInicio);
         //modificar ajusta la velocidad, negativo de derecha a izquierda, positivo de izquierda a derecha
         alpha += -1f;
     }
