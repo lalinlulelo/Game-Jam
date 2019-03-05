f229b784dd9e21a04f2f3fbb71a4aadcc376f6b4
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-05 20:22:05 +0100

    cambio en player

--- Extra Credits 1/Assets/Scripts/Player/Player.cs
+++ Extra Credits 1/Assets/Scripts/Player/Player.cs
@@ -6,11 +6,11 @@ using System;
 public class Player : MonoBehaviour
 {
     public float healthPoints=2;
-
+    private float maximoCorrer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maximoCorrer = this.GetComponent<Transform>().position.x+limite;
     }
 
     // Update is called once per frame
@@ -21,12 +21,11 @@ public class Player : MonoBehaviour
     public void TakeDamage(float damage)
     {
         healthPoints -= damage;
-
-        if (healthPoints <= 0)
-        {
-            this.GetComponent<Animator>().SetBool("Shot", true);
-            this.OnGameOver();
-        }
+        GetComponent<Transform>().position=new Vector3(GetComponent<Transform>().position.x-this.cuantoRetroceder, GetComponent<Transform>().position.y,
+            GetComponent<Transform>().position.z);
+        limite++;
+            //this.GetComponent<Animator>().SetBool("Shot", true);
+            //this.OnGameOver();        
     }
     
 
@@ -34,10 +33,18 @@ public class Player : MonoBehaviour
         Debug.Log("Game Over");
         UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
     }
-
+    public float cuantoAvanzar = 1f;
+    public float cuantoRetroceder = 1f;
+    public float limite=2f;
     public void ApplyEfect(Type t) {
         if (t == typeof(Orange)) {
             this.healthPoints += 1;
+            if (this.GetComponent<Transform>().position.x<(this.maximoCorrer-0.5))
+            {
+                GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + this.cuantoAvanzar, GetComponent<Transform>().position.y,
+                GetComponent<Transform>().position.z);
+                limite--;
+            }
         } else if (t == typeof(Coin)) {
             ApplicationModel.days += 10;
         }
