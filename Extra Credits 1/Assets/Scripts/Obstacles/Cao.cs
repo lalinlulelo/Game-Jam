ffc4221b4196d80b9568638fa1915184a29c0ab8
Lei Chen Xu <lei.chen.xu@outlook.es>
2019-03-07 13:18:31 +0100

    last changes

---
+++ Extra Credits 1/Assets/Scripts/Obstacles/Cao.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cao : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
